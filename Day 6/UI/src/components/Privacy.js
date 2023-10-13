9c0d00c030a18dd17f70953bb578de8e8779e65a
Dharun26 <110535794+Dharun26@users.noreply.github.com>
2023-10-13 14:53:15 +0530

    Add files via upload

---
+++ Day 6/UI/src/components/Privacy.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+      Creating a robust privacy policy is essential for any online service, including a train ticket booking system. In the digital age, where data privacy concerns are at the forefront, users want to know how their personal information is collected, used, and protected. A comprehensive privacy policy not only ensures legal compliance but also builds trust with your users. In this 500-word essay, we'll delve deeper into the importance of a privacy policy for a train ticket booking system.
+
+A privacy policy is a legal document that outlines how a business or service collects, processes, stores, and protects users' personal information. For a train ticket booking system, this personal information can include names, contact details, payment information, travel preferences, and more. It's crucial to have a privacy policy that explains what happens to this data.
+
+First and foremost, a privacy policy is a legal requirement in many jurisdictions. Laws such as the General Data Protection Regulation (GDPR) in Europe and the California Consumer Privacy Act (CCPA) in the United States mandate that businesses must inform users about their data practices. Failure to comply with these laws can result in hefty fines and legal consequences.
+
+But beyond legal compliance, a privacy policy serves as a transparency tool. It tells users what data you collect, why you collect it, and how it will be used. This transparency is vital for building trust with your users. When people know that their information is handled responsibly and with their consent, they are more likely to use your service.
+
+For a train ticket booking system, personal information is collected at various stages, from the initial booking to the payment process. Users need assurance that their data is secure and won't be misused. A privacy policy should explain the security measures in place, such as encryption and access controls, to protect user data.
+
+Moreover, a privacy policy should detail how long user data is retained. In the case of a train ticket booking system, there may be a need to retain booking and travel history for a certain period. The policy should specify this duration and the reasons behind it.
+
+Consent is another crucial aspect. Users should have the option to consent or opt out of marketing communications or the sharing of their data with third parties. Clear opt-in and opt-out mechanisms should be provided, and the privacy policy should explain how to exercise these choices.
+
+In the event of a data breach, a privacy policy should outline the procedures for notifying affected users. Timely and transparent communication during such incidents is essential to maintain trust.
+
+Additionally, a well-crafted privacy policy should be easy to understand. Avoid jargon and complex legal language. Use clear and concise explanations so that users can grasp the essence of your data practices without needing a legal degree.
+
+As technology evolves, so do data privacy concerns. Therefore, a privacy policy should be a living document that is regularly reviewed and updated to reflect changes in data handling practices, regulations, and technologies.
+
+In conclusion, a privacy policy is not just a legal obligation but a fundamental aspect of running a train ticket booking system. It demonstrates your commitment to protecting user data, fosters trust, and ensures compliance with data protection laws. It's a critical document that should be accessible, transparent, and regularly updated to meet the evolving landscape of data privacy. By prioritizing privacy, you not only comply with the law but also provide users with the peace of mind they deserve when using your service.
+
+
+
+
+
+      </p>
+      <p>
+      Indian Railway Catering and Tourism Corporation Limited  is committed to maintain the trust and
+confidence of visitors to the website and its Mobile Applications. Users may access
+or/and download the information or material outlined on www.irctc.co.in and its Mobile Applications only
+for personal and non-commercial use. The unauthorized use/ illegal use of the website/ mobile application
+is strictly prohibited.
+IRCTC’s eTicketing platform may use social media networks such as Facebook, Twitter etc. to share our
+information/videos for better convenience of the commuters and general public at large. IRCTC do not
+claim any responsibility nor shall any claim stand against IRCTC if any error/ inconvenience occurs while
+browsing it or any objection put on it by these site hosting organization/agencies at any time.
+Site Visit Data:
+This website/ mobile application records your visit and logs the following information for statistical
+purposes such as, your IP address, type of browser you use, type of operating system, date and time you
+access the site, webpages you have accessed, dates downloaded etc. This information may be used to
+track aggregate traffic patterns.
+IRCTC eTicketing website and mobile applications does not automatically capture the specific personal
+information from you, (like name, phone number or email address), that allows us to identify the end user
+individually.
+Cookies:
+This site uses cookies to give you a personalized experience on the website and ensure that it is
+functioning effectively. In order to identify your interest in our products, and to track the effectiveness
+of advertising served by us, such cookies are used. This website does not collect any personal
+information during this process.
+Browser controls:
+Majority of the browsers will allow you to see what cookies you have, and delete them on an individual
+basis or block cookies from any particular or all websites. Basically, cookies allow ease of access, by logging
+you in without having to type your login name each time (only your password is needed). We may also
+use such cookies to display any advertisement(s) to you while you are on the Website or to send you offers
+(or similar emails – provided you have not opted out of receiving such emails) focusing on products which
+may be of your interest.
+You should be aware that if the cookies are deleted, any preference you have set will be lost which
+includes your preference to opt-out from cookies, as this itself requires an opt- out cookie to have been
+set. Also, this may also lead to unavailability of some services.
+Automatic Logging of Session Data:
+Each time you access IRCTC eTicketing website / mobile applications, your session data gets logged.
+Session data may consist of various aspects like the IP address, operating system and type of browser
+software being used and the activities done by the User on the Website / Mobile application. We collect
+session data as it helps us analyse User’s choices, browsing pattern including the frequency of visits and
+duration for which a User is logged on. It also helps us diagnose problems with our servers and lets us
+
+
+better administer our systems. The aforesaid information cannot identify any User personally. However,
+it may be possible to determine a User's Internet Service Provider (ISP), and the approximate geographic
+location of User's point of connectivity through the above session data.
+Links to External Websites/Portals:
+At many places in this IRCTC eTicketing website / mobile applications, you may find links to other
+websites/portals. These links have been placed for your information and convenience. When a user
+selects a link to an external portal, the user leaves the IRCTC eTicketing website / mobile application and
+is subject to the privacy and security policies of the owners/sponsors of the external portal/website. IRCTC
+is not responsible for the contents and reliability of the linked websites and does not necessarily endorse
+the views expressed in them. Mere presence of the link or its listing on this website / mobile application
+should not be assumed as endorsement of any kind. IRCTC does not guarantee that linked websites
+comply with Indian Laws. We cannot guarantee that these links will work all the time and we have no
+control over availability of linked pages.
+
+      </p>
+      {/* Add more content here */}
+    </div>
+  );
+}
