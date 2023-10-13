c0394e9d754329785fadfdc587e5d138cc71ca75
Dharun26 <110535794+Dharun26@users.noreply.github.com>
2023-10-13 14:51:33 +0530

    Add files via upload

---
+++ Day 5/ui/src/components/Terms.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+export default function Terms() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>
+      Terms and Conditions for Railway Ticket Booking System
+Effective Date: [Date]
+<br></br>
+
+Welcome to our Railway Ticket Booking System (the "Service"). These Terms and Conditions outline the rules, rights, and responsibilities governing your use of the Service provided by [Your Company Name] ("we," "us," or "our"). By using the Service, you agree to comply with and be bound by these Terms and Conditions.
+
+<br></br>
+1. Acceptance of Terms<br></br>
+
+By using the Service, you acknowledge that you have read, understood, and agree to these Terms and Conditions. If you do not agree to these terms, please refrain from using the Service.
+<br></br>
+
+2. Ticket Booking and Payment<br></br>
+
+2.1 Booking Process<br></br>
+
+To book a train ticket, users must provide accurate and complete information, including passenger details and payment information.<br></br>
+
+
+Tickets are subject to availability, and we do not guarantee the availability of specific trains or seats.<br></br>
+
+
+2.2 Payment<br></br>
+
+Payment must be made using the available payment methods provided on the Service. We do not store credit card information, and all payments are processed securely.
+<br></br>
+
+Users are responsible for any fees, charges, or taxes associated with their bookings.
+<br></br>
+
+2.3 Cancellations and Refunds<br></br>
+
+Users may cancel their bookings subject to the cancellation policy of the respective railway operator. Refund eligibility is determined by the operator's policies.
+<br></br>
+
+Processing fees may apply to cancellations or modifications made by users.
+<br></br>
+
+3. Privacy and Data Security
+<br></br>
+3.1 User Data
+<br></br>
+We collect and process user data as outlined in our Privacy Policy. By using the Service, you consent to the collection and processing of your data.
+
+
+We take reasonable measures to protect user data, but we are not liable for any data breaches or unauthorized access beyond our control.
+
+<br></br>
+3.2 Communications
+<br></br>
+By using the Service, users agree to receive transactional and promotional communications, including emails and SMS messages, related to their bookings and our services. Users can opt out of marketing communications.<br></br>
+4. User Responsibilities<br></br>
+Users must provide accurate and up-to-date information during the booking process.
+
+Users are responsible for maintaining the confidentiality of their account credentials and are liable for any unauthorized use of their accounts.
+
+Users must abide by all applicable laws and regulations while using the Service.
+<br></br>
+5. Service Modifications and Availability<br></br>
+We reserve the right to modify, suspend, or discontinue the Service, or any part thereof, at any time without prior notice.
+
+We are not liable for any disruptions or outages that may affect the availability of the Service.
+<br></br>
+6. Limitation of Liability<br></br>
+We are not liable for any loss, damage, or inconvenience resulting from the use of the Service, including but not limited to booking errors, cancellations, delays, or service disruptions.
+
+Users are responsible for their travel arrangements, including connections and accommodations.
+<br></br>
+7. Intellectual Property<br></br>
+All content and materials on the Service, including logos, trademarks, and text, are protected by intellectual property rights and may not be used without our prior written consent.<br></br>
+8. Governing Law and Dispute Resolution<br></br>
+These Terms and Conditions are governed by and construed in accordance with the laws of [Your Jurisdiction]. Any disputes arising from or related to these terms shall be resolved through arbitration in accordance with the rules of the [Arbitration Organization].
+<br></br>
+9. Changes to Terms and Conditions<br></br>
+We reserve the right to update or modify these Terms and Conditions at any time. Updated terms will be effective upon posting on the Service.<br></br>
+10. Contact Information<br></br>
+If you have any questions or concerns regarding these Terms and Conditions or our Service, please contact us at [Your Contact Information].
+
+These Terms and Conditions constitute a legal agreement between you and [Your Company Name]. By using the Railway Ticket Booking System, you acknowledge and agree to abide by these terms. It is essential to review and understand these terms before using our Service, and we encourage you to keep a copy for your records.
+
+By creating these comprehensive terms and conditions, you establish clear guidelines for using your railway ticket booking system, helping to protect both your business and your users' interests while ensuring transparency and compliance with legal requirements.
+
+<br></br>
+
+
+
+      </p>
+     
+      {/* Add more terms and conditions content here */}
+    </div>
+  );
+}
---
+++ Day 5/ui/src/reducers/seatSelectionReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    selectedSeats: [],
+  };
+  
+  const seatSelectionReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'SELECT_SEATS':
+        return {
+          ...state,
+          selectedSeats: action.seats,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default seatSelectionReducer;
